008b3134deff0743af751d5e6686b3e7d1b684db
Olga Amitousa-Mankoy <olga.mankoy@gmail.com>
2025-01-11 17:53:06 +0100

    Init preprod TF

---
+++ tofu/preprod/main.tf
@@ -0,0 +1,29 @@
+# ===================================
+# Author: @wfamous / famousinvogue.com
+# Main script to manage infrastructure that will update our website data
+# ===================================
+
+# Terraform initial config
+terraform {
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.83.1"
+    }
+  }
+
+  backend "s3" {
+    bucket   = "tfstate-fiv-backend"
+    key      = "state/terraform.tfstate"
+    region   = "eu-west-3"
+  }
+}
+
+
+# AWS config
+provider "aws" {
+  region  = var.aws_region
+
+}
+
---
+++ tofu/preprod/variables.tf
@@ -0,0 +1,27 @@
+# ===================================
+# Variables
+# ===================================
+
+# environment
+variable "environment" {
+  description = "Environment of deployment"
+  type        = string
+}
+
+# AWS
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
