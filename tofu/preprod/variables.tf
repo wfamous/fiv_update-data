3440ea47871da048ed656909f3ceb57c5d98822e
Olga Amitousa-Mankoy <olga.mankoy@gmail.com>
2025-01-11 18:00:26 +0100

    Removed aws vars

--- tofu/preprod/variables.tf
+++ tofu/preprod/variables.tf
@@ -7,21 +7,3 @@ variable "environment" {
   description = "Environment of deployment"
   type        = string
 }
-
-# AWS
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
