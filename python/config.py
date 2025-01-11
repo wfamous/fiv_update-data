933d8a02b7347d32b973772a45340341550ee98a
Olga Amitousa-Mankoy <olga.mankoy@gmail.com>
2025-01-11 13:01:32 +0100

    Remove unused configs

--- python/config.py
+++ python/config.py
@@ -16,6 +16,3 @@ shopify_name = os.getenv("SHOPIFY_NAME")
 shopify_api_secret = os.getenv("SHOPIFY_API_SECRET")
 shopify_api_token = os.getenv("SHOPIFY_API_TOKEN")
 api_version  = os.getenv("SHOPIFY_VERSION")
-state = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
-redirect_uri = f'http://{shop_url}/auth/shopify/callback'
-scopes = ['read_products', 'read_orders']
