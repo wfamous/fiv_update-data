9e9658bfb177b4d537ffe6d2130f4b9941030853
Olga Amitousa-Mankoy <olga.mankoy@gmail.com>
2025-02-27 22:05:35 +0100

    Remove config and fix modules paths

--- tofu/preprod/main.tf
+++ tofu/preprod/main.tf
@@ -3,37 +3,20 @@
 # Main script to manage infrastructure that will update our website data
 # ===================================
 
-# Terraform initial config
-terraform {
-  required_providers {
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.83.1"
-    }
-
-
-  }
-  backend "s3" {
-      bucket         = "tfstate-fiv-backend-${var.environment}"
-      key            = "state/terraform.tfstate"
-      region         = "eu-west-3"
-  }
-}
 
 # ===================================
 #  Storages creation
 # ===================================
 
 module "aws_buckets" {
-  source          = "modules/aws_buckets"
-  private_buckets = var.private_buckets
+  source          = "./modules/aws_buckets"
+  private_buckets = var.aws_private_buckets
   environment     = var.environment
 }
 
 module "gcp_buckets" {
-  source          = "modules/gcp_buckets"
-  private_buckets = var.private_buckets
+  source          = "./modules/gcp_buckets"
+  private_buckets = var.aws_private_buckets
   gcp_region      = var.gcp_region
   gcp_project_id =  var.gcp_project_id
   environment     = var.environment
