c155b939929785d7054c92734f3594403385322a
Olga Amitousa-Mankoy <olga.mankoy@gmail.com>
2025-03-01 15:32:41 +0100

    Add placeholder object to create aws folders

--- tofu/preprod/modules/aws_buckets/main.tf
+++ tofu/preprod/modules/aws_buckets/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 # ===================================
 # Author: @wfamous / famousinvogue.com
 # Manage storages
@@ -15,5 +23,13 @@ resource "aws_s3_bucket" "private_content_buckets" {
   }
 }
 
-# can't create empty folder on aws
+resource "aws_s3_object" "private_buckets_subfolders" {
+  count  = length(var.aws_private_buckets_subfolders)
+  bucket = aws_s3_bucket.private_content_buckets[0].id
+  key    = "${var.aws_private_buckets_subfolders[count.index]}/readme.txt"
 
+  tags   = {
+    Name  = var.aws_private_buckets_subfolders[count.index]
+    Environment = var.environment
+  }
+}
