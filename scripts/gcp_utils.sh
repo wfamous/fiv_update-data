d3bef4ecd75b74aa8bc5a10592c5d81333f0b9ea
Olga Amitousa-Mankoy <olga.mankoy@gmail.com>
2025-03-01 15:04:23 +0100

    Add key file creation and SA login

--- scripts/gcp_utils.sh
+++ scripts/gcp_utils.sh
@@ -6,14 +6,14 @@
 
 # check if service account already exist
 function check_service_account() {
-  echo "Step : Check is Service Account Exist"
+  echo "Step : Check is Service Account exist"
   local SA_ACCOUNT_EMAIL=$1
 
   gcloud iam service-accounts list --format="value(email)" | grep -w "$SA_ACCOUNT_EMAIL" &> /dev/null
   return $?
 }
 
-# create service account
+# create service account and its key
 function create_service_account() {
   echo "Step : Create Service Account"
   local PROJECT_ID=$1
@@ -24,11 +24,28 @@ function create_service_account() {
   SA_ACCOUNT_EMAIL="$SA_NAME@$PROJECT_ID.$SA_ACCOUNT_HOST"
 
   if check_service_account "$SA_ACCOUNT_EMAIL"; then
-      echo "--Service account '$SA_ACCOUNT_EMAIL' already exists."
+      echo "--Service account already exists."
+      create_sa_key_file $SA_ACCOUNT_EMAIL
   else
       echo "--Creating service account '$SA_ACCOUNT_EMAIL' ..."
       gcloud iam service-accounts create "$SA_NAME" --display-name "Terraform Service Account"
       echo "✅ Service account created successfully!"
+
+      create_sa_key_file $SA_ACCOUNT_EMAIL
+  fi
+}
+
+function create_sa_key_file(){
+  local SA_EMAIL=$1
+   echo "Step : Creating key file for SA account"
+
+  if [ -f "../external/sa-key.json" ]; then
+    echo "A key file exist for this account."
+  else
+    echo "--Creating key for account"
+     gcloud iam service-accounts keys create sa-key.json \
+    --iam-account=$SA_EMAIL
+    mv sa-key.json ../external
   fi
 }
 
@@ -43,11 +60,17 @@ function bind_sa_iam_policy(){
 
    # assign multiple roles to service account
   for ROLE in "${IAM_ROLES[@]}"; do
-    echo "ROle is : " "$ROLE"
     gcloud projects add-iam-policy-binding "$PROJECT_ID" \
     --member=serviceAccount:"$SA_ACCOUNT_EMAIL" \
     --role="$ROLE"
   done
+}
+
+# for local use only
+function sa_login(){
+  local KEY_FILE=$1
+  echo "Step : Log in SA account"
+  gcloud auth activate-service-account --key-file="$KEY_FILE"
 }
 
 # creates buckets
@@ -64,7 +87,7 @@ function create_tf_backend_bucket(){
   if check_bucket_name ${BUCKET_NAMES[@]} $ENV; then
     echo "--Backend bucket already exists."
   else
-    echo "--Creating backend bucket exists."
+    echo "--Creating backend bucket"
     gcloud storage buckets create gs://"$BUCKET_NAME-$ENV" \
     --location="$BUCKET_LOCATION" --public-access-prevention \
     --uniform-bucket-level-access --default-storage-class="$CLASS_STORAGE"
@@ -79,7 +102,7 @@ function check_bucket_name(){
 
   for BUCKET in "${BUCKET_NAMES[@]}"; do
     if gcloud storage buckets list --format="value(name)" | grep -wq "$BUCKET-$ENV"; then
-      echo "Bucket already exists."
+      return 0
     else
       return 1
     fi
